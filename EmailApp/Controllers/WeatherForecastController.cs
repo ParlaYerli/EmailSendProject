925f1d89db58ca5161b1dea8b54fe0fea5332cac
Ayşe Parla Yerli <parla.yerli@demirorenteknoloji.com>
2020-06-29 13:59:59 +0300

    Add project files.

---
+++ EmailApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmailService;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EmailApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly IEmailSender emailSender;
+
+        public WeatherForecastController(IEmailSender emailSender)
+        {
+            this.emailSender = emailSender;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            var message = new Message(new string[] { "codemazetest@mailinator.com" }, "test mail", "this is the content from our email ");
+            emailSender.SendEmail(message);
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
